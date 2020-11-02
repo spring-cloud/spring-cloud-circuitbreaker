e9dfea29a5b6cdcd8770d4ad102f30af626ec16f
Srinivasa Vasu <srinivasan.surprise@gmail.com>
2020-11-03 05:28:21 +0800

    Separate ConditionalOnProperty for blocking and reactive (#63)
    Signed-off-by: Srinivasa Vasu <srinivasan.surprise@gmail.com>
    
    Co-authored-by: Spencer Gibb <sgibb@pivotal.io>

--- spring-cloud-circuitbreaker-resilience4j/src/main/java/org/springframework/cloud/circuitbreaker/resilience4j/ReactiveResilience4JAutoConfiguration.java
+++ spring-cloud-circuitbreaker-resilience4j/src/main/java/org/springframework/cloud/circuitbreaker/resilience4j/ReactiveResilience4JAutoConfiguration.java
@@ -41,7 +41,10 @@ import org.springframework.context.annotation.Configuration;
 @Configuration(proxyBeanMethods = false)
 @ConditionalOnClass(name = { "reactor.core.publisher.Mono", "reactor.core.publisher.Flux",
 		"io.github.resilience4j.reactor.circuitbreaker.operator.CircuitBreakerOperator" })
-@ConditionalOnProperty(name = "spring.cloud.circuitbreaker.resilience4j.enabled", matchIfMissing = true)
+@ConditionalOnProperty(
+		name = { "spring.cloud.circuitbreaker.resilience4j.enabled",
+				"spring.cloud.circuitbreaker.resilience4j.reactive.enabled" },
+		matchIfMissing = true)
 public class ReactiveResilience4JAutoConfiguration {
 
 	@Autowired(required = false)
